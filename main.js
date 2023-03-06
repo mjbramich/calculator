d8125fde3d5f58dccf0ae4dc5ec32885c65c961f
Matthew Bramich <matt_bramich@hotmail.com>
2023-03-06 15:43:12 +1100

    create basic math operator functions

---
+++ main.js
@@ -0,0 +1,8 @@
+
+// Operator Functions
+const add = (a , b) => a + b
+const subtract = (a, b) => a - b
+const multiply = (a, b) => a * b
+const divide = (a, b) => a / b
+
+const operate = (a, b, operator ) => operator(a,b)
